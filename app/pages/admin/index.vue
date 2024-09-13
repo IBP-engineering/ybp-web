f25240c9e494203bfa6adda70bed4a5c2f0fa8f2
Kalwabed Rizki <49640654+kalwabed@users.noreply.github.com>
2024-09-13 20:44:02 +0700

    styles: add icon

--- app/pages/admin/index.vue
+++ app/pages/admin/index.vue
@@ -12,7 +12,7 @@ definePageMeta({
     <PageHeader title="Articles" />
 
     <div class="mt-8 w-full max-w-screen-lg grid grid-cols-1 gap-8 mx-auto">
-      <div v-for="_ in [1, 2, 3]" class="flex gap-4">
+      <div v-for="v in [1, 2, 3]" :key="v.toString()" class="flex gap-4">
         <div>
           <img
             src="https://picsum.photos/250/130"
@@ -23,16 +23,26 @@ definePageMeta({
           />
         </div>
         <section>
-          <h2 class="text-2xl font-semibold leading-relaxed">
+          <NuxtLink
+            class="text-2xl font-semibold hover:text-primary-600 leading-relaxed"
+            to="/articles/hehe"
+          >
             Ketika kekuasaan disalahgunakan
-          </h2>
-          <p class="text-gray-600">oleh Kalwabed Rizki</p>
+          </NuxtLink>
+          <div class="text-gray-600">
+            <span> oleh </span>
+            <ULink
+              to="/users/123"
+              inactive-class="underline hover:text-primary-600"
+              >Kalwabed Rizki</ULink
+            >
+          </div>
           <p class="text-gray-600">17 Agu 2024, 11:23</p>
           <NuxtLink
-            class="text-gray-600 hover:underline mt-4 block"
+            class="text-gray-600 hover:underline mt-4 flex items-center"
             to="/articles/hehe"
-            >Detail -></NuxtLink
-          >
+            >Detail <UIcon name="i-heroicons:arrow-up-right-solid"
+          /></NuxtLink>
         </section>
       </div>
     </div>
