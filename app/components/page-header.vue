265a75f9933bf2ea83913d2389f6d710b9f14e63
Kalwabed Rizki <49640654+kalwabed@users.noreply.github.com>
2024-09-15 11:45:07 +0700

    fix: nav size

--- app/components/page-header.vue
+++ app/components/page-header.vue
@@ -43,7 +43,7 @@ const links = [
   <div class="w-full border-b bg-white">
     <nav
       v-if="mode === 'list'"
-      class="hidden items-center gap-2 border-b py-2 md:flex"
+      class="mx-auto hidden max-w-screen-xl items-center gap-2 border-b py-2 md:flex"
     >
       <div v-for="link in links" :key="link.icon">
         <NuxtLink
