36c424cb625ef76f6a811abb8801c918bf2a9407
Kalwabed Rizki <49640654+kalwabed@users.noreply.github.com>
2024-09-17 22:20:07 +0700

    styles: add sparkles for badge

--- app/components/role-badge.vue
+++ app/components/role-badge.vue
@@ -1,8 +1,9 @@
 <template>
   <p
-    class="w-fit rounded border border-purple-700 bg-purple-500 px-1 text-xs font-medium text-white"
+    class="inline-flex w-fit items-center gap-1 rounded border border-purple-700 bg-purple-500 px-1 text-xs font-medium text-white"
     title="Role: Member"
   >
+    <UIcon name="i-heroicons:sparkles-20-solid" />
     Member
   </p>
 </template>
