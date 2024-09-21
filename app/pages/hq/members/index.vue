aecfc2a2d7a77f5203676451b4a6eef43a969a8c
Kalwabed Rizki <49640654+kalwabed@users.noreply.github.com>
2024-09-21 16:50:06 +0700

    ref: remove feat to add member

--- app/pages/hq/members/index.vue
+++ app/pages/hq/members/index.vue
@@ -1,16 +1,4 @@
 <script setup lang="ts">
-import {
-  nonEmpty,
-  object,
-  pipe,
-  safeParser,
-  string,
-  trim,
-  forward,
-  partialCheck,
-  type InferInput,
-} from 'valibot'
-import type { FormSubmitEvent } from '#ui/types'
 import type { User } from '~/types/entities'
 
 definePageMeta({
@@ -21,62 +9,17 @@ useHead({
   title: 'Members',
 })
 
-const schema = pipe(
-  object({
-    username: usernameValidator,
-    email: emailValidator,
-    displayName: pipe(string(), nonEmpty('Mohon masukkan nama anda')),
-    password: passwordValidator,
-    repeatPassword: pipe(
-      string(),
-      trim(),
-      nonEmpty('Mohon masukkan konfirmasi password anda'),
-    ),
-  }),
-  forward(
-    partialCheck(
-      [['password'], ['repeatPassword']],
-      input => input.password === input.repeatPassword,
-      'Konfirmasi password tidak sesuai',
-    ),
-    ['repeatPassword'],
-  ),
-)
-
-type Schema = InferInput<typeof schema>
-
-const isOpenModalNewMember = ref(false)
-const isLoading = ref(false)
-const state = reactive({
-  username: '',
-  displayName: '',
-  email: '',
-  password: '',
-  repeatPassword: '',
-})
 const supabase = useSupabaseClient()
 const { data: members } = (await supabase.from('users').select()) as {
   data: User[]
-}
-
-async function addNewMember(event: FormSubmitEvent<Schema>) {
-  console.log(event)
-}
-
-function openModalNewMember() {
-  isOpenModalNewMember.value = true
 }
 </script>
 
 <template>
   <div>
-    <PageHeader
-      button-text="Tambah baru"
-      @btn-click="openModalNewMember"
-      title="Members"
-    />
+    <PageHeader title="Members" />
     <div class="mx-auto mt-4 w-full max-w-screen-xl px-4">
-      <p>Total: 25</p>
+      <p>Total: {{ members.length }}</p>
 
       <div class="mt-2 block">
         <ul class="grid grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-3">
@@ -106,79 +49,5 @@ function openModalNewMember() {
         </ul>
       </div>
     </div>
-
-    <UModal v-model="isOpenModalNewMember" prevent-close>
-      <UCard
-        :ui="{
-          ring: '',
-          divide: 'divide-y divide-gray-100 dark:divide-gray-800',
-        }"
-      >
-        <template #header>
-          <div class="flex items-center justify-between">
-            <h3
-              class="text-base font-semibold leading-6 text-gray-900 dark:text-white"
-            >
-              Add new member
-            </h3>
-            <UButton
-              color="gray"
-              variant="ghost"
-              icon="i-heroicons-x-mark-20-solid"
-              class="-my-1"
-              @click="isOpenModalNewMember = false"
-            />
-          </div>
-        </template>
-
-        <UForm
-          :schema="safeParser(schema)"
-          :state="state"
-          @submit="addNewMember"
-          class="flex w-full flex-col"
-        >
-          <UFormGroup required label="Nama" name="displayName">
-            <UInput v-model="state.displayName" :loading="isLoading" />
-          </UFormGroup>
-          <UFormGroup required class="mt-4" label="Username" name="username">
-            <UInput v-model="state.username" :loading="isLoading" type="text" />
-          </UFormGroup>
-          <UFormGroup required class="mt-4" label="Email" name="email">
-            <UInput v-model="state.email" :loading="isLoading" type="text" />
-          </UFormGroup>
-          <UFormGroup required class="mt-4" label="Password" name="password">
-            <UInput
-              v-model="state.password"
-              :loading="isLoading"
-              type="password"
-            />
-          </UFormGroup>
-          <UFormGroup
-            required
-            class="mt-4"
-            label="Konfirmasi password"
-            name="repeatPassword"
-          >
-            <UInput
-              v-model="state.repeatPassword"
-              :loading="isLoading"
-              type="password"
-            />
-          </UFormGroup>
-
-          <div class="mt-6 flex items-center justify-end gap-4">
-            <UButton
-              @click="isOpenModalNewMember = false"
-              variant="ghost"
-              color="gray"
-              :loading="isLoading"
-            >
-              Batal
-            </UButton>
-            <UButton :loading="isLoading" type="submit"> Simpan </UButton>
-          </div>
-        </UForm>
-      </UCard>
-    </UModal>
   </div>
 </template>
