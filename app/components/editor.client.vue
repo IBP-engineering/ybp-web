a842f8ca0fc5597d894aa74f3ec9f1803b5c7719
Kalwabed Rizki <49640654+kalwabed@users.noreply.github.com>
2024-09-26 20:32:14 +0700

    feat: initial story with title

--- app/components/editor.client.vue
+++ app/components/editor.client.vue
@@ -7,6 +7,7 @@ import StarterKit from '@tiptap/starter-kit'
 const editorModel = defineModel('editor')
 
 const editor = ref<Editor | null>(null)
+const showTextOptions = ref(false)
 
 watch(
   () => editor.value?.getHTML(),
@@ -28,6 +29,14 @@ onMounted(() => {
     onUpdate: () => {
       editorModel.value = editor.value.getHTML()
     },
+    onFocus: () => {
+      showTextOptions.value = true
+      console.log('focus')
+    },
+    onBlur: () => {
+      showTextOptions.value = false
+      console.log('blur')
+    },
   })
 })
 
@@ -38,140 +47,137 @@ onUnmounted(() => {
 
 <template>
   <div>
-    <div
-      class="mb-2 flex w-fit items-center gap-2 rounded-lg border border-gray-300 bg-gray-50 px-2 py-1"
-    >
-      <UTooltip text="Heading 1" :popper="{ arrow: true, placement: 'top' }">
-        <UButton
-          :variant="
-            editor.isActive('heading', { level: 1 }) ? 'solid' : 'ghost'
-          "
-          color="black"
-          size="xs"
-          icon="i-ph:text-h-one"
-          @click="editor.chain().focus().toggleHeading({ level: 1 }).run()"
-          :disabled="
-            !editor.can().chain().focus().toggleHeading({ level: 1 }).run()
-          "
-          square
-        />
-      </UTooltip>
-      <UTooltip text="Heading 2" :popper="{ arrow: true, placement: 'top' }">
-        <UButton
-          :variant="
-            editor.isActive('heading', { level: 2 }) ? 'solid' : 'ghost'
-          "
-          color="black"
-          size="xs"
-          icon="i-ph:text-h-two"
-          @click="editor.chain().focus().toggleHeading({ level: 2 }).run()"
-          :disabled="
-            !editor.can().chain().focus().toggleHeading({ level: 2 }).run()
-          "
-          square
-        />
-      </UTooltip>
-      <UTooltip text="Heading 3" :popper="{ arrow: true, placement: 'top' }">
-        <UButton
-          :variant="
-            editor.isActive('heading', { level: 3 }) ? 'solid' : 'ghost'
-          "
-          color="black"
-          size="xs"
-          icon="i-ph:text-h-three"
-          @click="editor.chain().focus().toggleHeading({ level: 3 }).run()"
-          :disabled="
-            !editor.can().chain().focus().toggleHeading({ level: 3 }).run()
-          "
-          square
-        />
-      </UTooltip>
-      <UTooltip text="Bold" :popper="{ arrow: true, placement: 'top' }">
-        <UButton
-          :variant="editor.isActive('bold') ? 'solid' : 'ghost'"
-          color="black"
-          size="xs"
-          icon="i-ph:text-bolder"
-          @click="editor.chain().focus().toggleBold().run()"
-          :disabled="!editor.can().chain().focus().toggleBold().run()"
-          square
-        />
-      </UTooltip>
-      <UTooltip text="Italic" :popper="{ arrow: true, placement: 'top' }">
-        <UButton
-          :variant="editor.isActive('italic') ? 'solid' : 'ghost'"
-          color="black"
-          size="xs"
-          icon="i-ph:text-italic"
-          @click="editor.chain().focus().toggleItalic().run()"
-          :disabled="!editor.can().chain().focus().toggleItalic().run()"
-          square
-        />
-      </UTooltip>
-      <UTooltip
-        text="Strike through"
-        :popper="{ arrow: true, placement: 'top' }"
+    <Transition>
+      <div
+        class="mb-2 flex w-fit items-center gap-2 rounded-lg border border-gray-300 bg-gray-50 px-2 py-1"
+        v-if="showTextOptions"
       >
-        <UButton
-          :variant="editor.isActive('strike') ? 'solid' : 'ghost'"
-          color="black"
-          size="xs"
-          icon="i-ph:text-strikethrough"
-          @click="editor.chain().focus().toggleStrike().run()"
-          :disabled="!editor.can().chain().focus().toggleStrike().run()"
-          square
-        />
-      </UTooltip>
-      <UTooltip text="Code" :popper="{ arrow: true, placement: 'top' }">
-        <UButton
-          :variant="editor.isActive('code') ? 'solid' : 'ghost'"
-          color="black"
-          size="xs"
-          icon="i-ph:code"
-          @click="editor.chain().focus().toggleCode().run()"
-          :disabled="!editor.can().chain().focus().toggleCode().run()"
-          square
-        />
-      </UTooltip>
-      <UTooltip text="Block quote" :popper="{ arrow: true, placement: 'top' }">
-        <UButton
-          :variant="editor.isActive('blockquote') ? 'solid' : 'ghost'"
-          color="black"
-          size="xs"
-          icon="i-ph:quotes"
-          @click="editor.chain().focus().toggleBlockquote().run()"
-          :disabled="!editor.can().chain().focus().toggleBlockquote().run()"
-          square
-        />
-      </UTooltip>
-      <UTooltip text="Bullet list" :popper="{ arrow: true, placement: 'top' }">
-        <UButton
-          :variant="editor.isActive('bulletList') ? 'solid' : 'ghost'"
-          color="black"
-          size="xs"
-          icon="i-ph:list-bullets"
-          @click="editor.chain().focus().toggleBulletList().run()"
-          :disabled="!editor.can().chain().focus().toggleBulletList().run()"
-          square
-        />
-      </UTooltip>
-      <UTooltip text="Ordered list" :popper="{ arrow: true, placement: 'top' }">
-        <UButton
-          :variant="editor.isActive('orderedList') ? 'solid' : 'ghost'"
-          color="black"
-          size="xs"
-          icon="i-ph:list-numbers"
-          @click="editor.chain().focus().toggleOrderedList().run()"
-          :disabled="!editor.can().chain().focus().toggleOrderedList().run()"
-          square
-        />
-      </UTooltip>
-    </div>
+        <UTooltip text="Heading 2" :popper="{ arrow: true, placement: 'top' }">
+          <UButton
+            :variant="
+              editor.isActive('heading', { level: 2 }) ? 'solid' : 'ghost'
+            "
+            color="black"
+            size="xs"
+            icon="i-ph:text-h-two"
+            @click="editor.chain().focus().toggleHeading({ level: 2 }).run()"
+            :disabled="
+              !editor.can().chain().focus().toggleHeading({ level: 2 }).run()
+            "
+            square
+          />
+        </UTooltip>
+        <UTooltip text="Heading 3" :popper="{ arrow: true, placement: 'top' }">
+          <UButton
+            :variant="
+              editor.isActive('heading', { level: 3 }) ? 'solid' : 'ghost'
+            "
+            color="black"
+            size="xs"
+            icon="i-ph:text-h-three"
+            @click="editor.chain().focus().toggleHeading({ level: 3 }).run()"
+            :disabled="
+              !editor.can().chain().focus().toggleHeading({ level: 3 }).run()
+            "
+            square
+          />
+        </UTooltip>
+        <UTooltip text="Bold" :popper="{ arrow: true, placement: 'top' }">
+          <UButton
+            :variant="editor.isActive('bold') ? 'solid' : 'ghost'"
+            color="black"
+            size="xs"
+            icon="i-ph:text-bolder"
+            @click="editor.chain().focus().toggleBold().run()"
+            :disabled="!editor.can().chain().focus().toggleBold().run()"
+            square
+          />
+        </UTooltip>
+        <UTooltip text="Italic" :popper="{ arrow: true, placement: 'top' }">
+          <UButton
+            :variant="editor.isActive('italic') ? 'solid' : 'ghost'"
+            color="black"
+            size="xs"
+            icon="i-ph:text-italic"
+            @click="editor.chain().focus().toggleItalic().run()"
+            :disabled="!editor.can().chain().focus().toggleItalic().run()"
+            square
+          />
+        </UTooltip>
+        <UTooltip
+          text="Strike through"
+          :popper="{ arrow: true, placement: 'top' }"
+        >
+          <UButton
+            :variant="editor.isActive('strike') ? 'solid' : 'ghost'"
+            color="black"
+            size="xs"
+            icon="i-ph:text-strikethrough"
+            @click="editor.chain().focus().toggleStrike().run()"
+            :disabled="!editor.can().chain().focus().toggleStrike().run()"
+            square
+          />
+        </UTooltip>
+        <UTooltip text="Code" :popper="{ arrow: true, placement: 'top' }">
+          <UButton
+            :variant="editor.isActive('code') ? 'solid' : 'ghost'"
+            color="black"
+            size="xs"
+            icon="i-ph:code"
+            @click="editor.chain().focus().toggleCode().run()"
+            :disabled="!editor.can().chain().focus().toggleCode().run()"
+            square
+          />
+        </UTooltip>
+        <UTooltip
+          text="Block quote"
+          :popper="{ arrow: true, placement: 'top' }"
+        >
+          <UButton
+            :variant="editor.isActive('blockquote') ? 'solid' : 'ghost'"
+            color="black"
+            size="xs"
+            icon="i-ph:quotes"
+            @click="editor.chain().focus().toggleBlockquote().run()"
+            :disabled="!editor.can().chain().focus().toggleBlockquote().run()"
+            square
+          />
+        </UTooltip>
+        <UTooltip
+          text="Bullet list"
+          :popper="{ arrow: true, placement: 'top' }"
+        >
+          <UButton
+            :variant="editor.isActive('bulletList') ? 'solid' : 'ghost'"
+            color="black"
+            size="xs"
+            icon="i-ph:list-bullets"
+            @click="editor.chain().focus().toggleBulletList().run()"
+            :disabled="!editor.can().chain().focus().toggleBulletList().run()"
+            square
+          />
+        </UTooltip>
+        <UTooltip
+          text="Ordered list"
+          :popper="{ arrow: true, placement: 'top' }"
+        >
+          <UButton
+            :variant="editor.isActive('orderedList') ? 'solid' : 'ghost'"
+            color="black"
+            size="xs"
+            icon="i-ph:list-numbers"
+            @click="editor.chain().focus().toggleOrderedList().run()"
+            :disabled="!editor.can().chain().focus().toggleOrderedList().run()"
+            square
+          />
+        </UTooltip>
+      </div>
+    </Transition>
 
     <div
       class="rounded-lg border border-gray-300 bg-gray-50 p-4 transition hover:border-gray-400"
     >
-      <EditorContent v-model="editorModel" :editor="editor"> </EditorContent>
+      <EditorContent v-model="editorModel" :editor="editor" />
     </div>
   </div>
 </template>
@@ -216,23 +222,33 @@ onUnmounted(() => {
   h4,
   h5,
   h6 {
+    line-height: 1.1;
     margin-top: 2.5rem;
     text-wrap: pretty;
   }
 
+  h1,
+  h2 {
+    margin-top: 3.5rem;
+    margin-bottom: 1.5rem;
+  }
+
   h1 {
     font-size: 1.875rem;
-    line-height: 2.25rem;
   }
 
   h2 {
     font-size: 1.5rem;
-    line-height: 2rem;
   }
 
   h3 {
     font-size: 1.25rem;
-    line-height: 1.75rem;
+  }
+
+  h4,
+  h5,
+  h6 {
+    font-size: 1rem;
   }
 
   /* Code and preformatted text styles */
@@ -245,5 +261,15 @@ onUnmounted(() => {
     padding: 0.25em 0.3em;
     font-family: 'Courier New', Courier, monospace;
   }
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.3s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
 }
 </style>
--- app/pages/new.vue
+++ app/pages/new.vue
@@ -1,14 +1,27 @@
 <script setup lang="ts">
-const content = ref('<h1>Jika saya pergi sendiri</h1>')
+const content = ref('<p>Pada suatu masa...</p>')
+const title = ref('Tanpa judul')
 </script>
 
 <template>
   <div class="mx-auto w-full max-w-screen-xl">
     <p>new page</p>
-    <div class="mt-12">
+    <div class="mx-auto mt-12 max-w-screen-lg">
+      <UInput
+        placeholder="Judulnyaaa"
+        variant="none"
+        color="gray"
+        input-class="mb-2 text-4xl"
+        v-model="title"
+        required
+      />
       <Editor v-model:editor="content" />
+      <div class="mt-4 flex items-center justify-end gap-4">
+        <UButton icon="i-heroicons:chevron-left" variant="ghost"
+          >Kembali</UButton
+        >
+        <UButton>Simpan</UButton>
+      </div>
     </div>
-
-    <code>{{ content }}</code>
   </div>
 </template>
