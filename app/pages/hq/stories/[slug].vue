65642e37e5503de8e471d250e76d13133fbe0205
Kalwabed Rizki <49640654+kalwabed@users.noreply.github.com>
2024-09-29 20:42:31 +0700

    feat: detail of story

--- app/pages/hq/stories/[slug].vue
+++ app/pages/hq/stories/[slug].vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
+import { format } from '@formkit/tempo'
 import type { Database } from '~/types/database.types'
+import type { StoryStatus } from '~/types/entities'
 
 definePageMeta({
   layout: 'dashboard',
@@ -14,6 +16,7 @@ const { data: story } = await useAsyncData(`hq/stories/${slug}`, async () => {
     .from('stories')
     .select(
       `*, 
+      author:users(display_name, username),
       tags:story_tags!id(tag_id(*)), 
       status:story_statuses!id(*)
       `,
@@ -28,6 +31,11 @@ const { data: story } = await useAsyncData(`hq/stories/${slug}`, async () => {
 
   return {
     ...data,
+    cover_path: data.cover_path
+      ? supabase.storage.from('story-cover').getPublicUrl(data.cover_path).data
+          .publicUrl
+      : null,
+    status: data.status as unknown as StoryStatus,
     tags: data.tags.map(tag => tag.tag_id) as unknown as {
       id: string
       description: string
@@ -41,25 +49,44 @@ const { data: story } = await useAsyncData(`hq/stories/${slug}`, async () => {
 <template>
   <div>
     <PageHeader title="Detail story" back-button-text="Stories" mode="detail" />
-    <div class="mx-auto w-full max-w-screen-xl px-4 md:px-0">
-      <h2 class="text-4xl font-bold leading-relaxed">{{ story.title }}</h2>
-      <div class="flex gap-2">
-        <div
-          class="rounded border border-gray-300 bg-gray-200 px-2"
-          v-for="tag in story.tags"
-          :key="tag.id"
-        >
-          <UTooltip :title="tag.title" :text="tag.description">
-            <b>#{{ tag?.slug }}</b>
-          </UTooltip>
+    <div class="mx-auto mt-8 w-full max-w-screen-xl px-4 md:px-0">
+      <div class="flex gap-4 border-b border-gray-400 pb-2">
+        <img
+          v-if="story.cover_path"
+          :src="story.cover_path"
+          height="300"
+          class="h-[200px] w-full md:w-[400px]"
+          alt="Story cover"
+        />
+        <div class="flex flex-col">
+          <StoryBadgeStatus :status="story.status" />
+          <h2 class="text-3xl font-bold leading-relaxed">{{ story.title }}</h2>
+          <div v-if="story.tags?.length > 0" class="mb-4 flex gap-2">
+            <div
+              class="rounded border border-gray-300 bg-gray-200 px-2"
+              v-for="tag in story.tags"
+              :key="tag.id"
+            >
+              <UTooltip :title="tag.title" :text="tag.description">
+                <b>#{{ tag?.slug }}</b>
+              </UTooltip>
+            </div>
+          </div>
+          <p class="text-gray-600">
+            Ditulis oleh
+            {{ story.author.display_name }}, pada
+            {{ format(story.created_at, 'DD MMM YYYY, HH:mm', 'id') }}
+          </p>
+          <p class="text-gray-600">
+            Update terakhir pada
+            {{ format(story.updated_at, 'DD MMM YYYY, HH:mm', 'id') }}
+          </p>
         </div>
       </div>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
-        dolor, modi accusamus neque veritatis iusto, sapiente quae qui, beatae
-        expedita porro dignissimos cum rerum autem ipsam cumque vero dolorum!
-        Necessitatibus.
-      </p>
+
+      <div class="mt-8">
+        <LazyProseRender :content="story.content" />
+      </div>
     </div>
   </div>
 </template>
--- app/types/entities.ts
+++ app/types/entities.ts
@@ -3,3 +3,4 @@ import type { Database } from './database.types'
 export type User = Database['public']['Tables']['users']['Row']
 export type Tag = Database['public']['Tables']['tags']['Row']
 export type Story = Database['public']['Tables']['stories']['Row']
+export type StoryStatus = Database['public']['Tables']['story_statuses']['Row']
