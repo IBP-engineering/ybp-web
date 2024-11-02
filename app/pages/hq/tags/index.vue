1bbecd3c43815807140616acba28a8cd66b1a61e
Kalwabed Rizki <49640654+kalwabed@users.noreply.github.com>
2024-11-02 16:31:58 +0700

    feat: add new tag

--- app/pages/hq/tags/index.vue
+++ app/pages/hq/tags/index.vue
@@ -22,8 +22,10 @@ type Schema = v.InferOutput<typeof schema>
 
 const toast = useToast()
 const supabase = useSupabaseClient()
+const user = useSupabaseUser()
 const isOpenDetail = ref(false)
 const selectedTagId = ref(null)
+const isLoading = ref(false)
 const state = reactive<Schema>({
   title: '',
   description: '',
@@ -39,7 +41,7 @@ const { data: tags, refresh } = await useAsyncData('hq/tags', async () => {
   return data ?? []
 })
 
-const { data: tag } = await useLazyAsyncData(
+const { data: tag, status: tagDetailStatus } = await useLazyAsyncData(
   'hq/tags/detail',
   async () => {
     if (selectedTagId.value) {
@@ -70,9 +72,10 @@ const { data: tag } = await useLazyAsyncData(
 async function onSubmit(event: FormSubmitEvent<Schema>) {
   const data = event.data
   const isUpdating = Boolean(selectedTagId.value)
-  const slug = toSlug(data.title, false)
 
   try {
+    isLoading.value = true
+    const slug = toSlug(data.title, false)
     if (isUpdating) {
       await supabase
         .from('tags')
@@ -87,18 +90,33 @@ async function onSubmit(event: FormSubmitEvent<Schema>) {
         title: 'Berhasil mengubah tag',
         color: 'green',
       })
+    } else {
+      await supabase.from('tags').insert({
+        title: data.title,
+        description: data.description,
+        is_active: data.isActive,
+        slug,
+        created_by: user.value.id,
+      })
+      toast.add({
+        title: 'Berhasil menambahkan tag',
+        color: 'green',
+      })
     }
 
     closeTagDetail()
   } catch (error) {
     console.error(error)
   } finally {
+    isLoading.value = false
     refresh()
   }
 }
 
 const openTagDetail = (id?: string) => {
-  selectedTagId.value = id
+  if (id) {
+    selectedTagId.value = id
+  }
   isOpenDetail.value = true
 }
 
@@ -115,7 +133,14 @@ const closeTagDetail = () => {
   <div>
     <PageHeader title="Tags" />
     <div class="mx-auto mt-4 w-full max-w-screen-xl px-4">
-      <p>Total: {{ tags.length ?? 0 }}</p>
+      <div class="flex items-center justify-between">
+        <UButton
+          trailing-icon="heroicons:plus-20-solid"
+          @click="() => openTagDetail()"
+          >Tambah</UButton
+        >
+        <small>Total: {{ tags.length ?? 0 }}</small>
+      </div>
 
       <div class="mt-8 grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4">
         <button
@@ -141,7 +166,7 @@ const closeTagDetail = () => {
       </div>
     </div>
 
-    <USlideover v-model="isOpenDetail">
+    <USlideover v-model="isOpenDetail" @close="closeTagDetail">
       <UCard
         class="flex flex-1 flex-col"
         :ui="{
@@ -155,7 +180,10 @@ const closeTagDetail = () => {
             <h3
               class="text-base font-semibold leading-6 text-gray-900 dark:text-white"
             >
-              Edit tag #{{ tag?.slug }}
+              <template v-if="selectedTagId">
+                Edit tag #{{ tag?.slug }}
+              </template>
+              <template v-else>Tambah tag baru</template>
             </h3>
             <UButton
               color="gray"
@@ -179,15 +207,25 @@ const closeTagDetail = () => {
             description="Akan otomatis mengubah slug"
             name="title"
           >
-            <UInput v-model="state.title" icon="heroicons:hashtag-20-solid" />
+            <UInput
+              :loading="tagDetailStatus === 'pending'"
+              v-model="state.title"
+              icon="heroicons:hashtag-20-solid"
+            />
           </UFormGroup>
 
           <UFormGroup label="Deskripsi" required name="description">
-            <UTextarea v-model="state.description" />
+            <UTextarea
+              :loading="tagDetailStatus === 'pending'"
+              v-model="state.description"
+            />
           </UFormGroup>
 
           <UFormGroup label="Aktif" required name="isActive">
-            <UToggle v-model="state.isActive" />
+            <UToggle
+              :loading="tagDetailStatus === 'pending'"
+              v-model="state.isActive"
+            />
           </UFormGroup>
 
           <div v-if="tag" class="text-sm text-gray-600">
@@ -201,7 +239,7 @@ const closeTagDetail = () => {
                 format(
                   new Date(tag?.created_at.toString()),
                   {
-                    date: 'full',
+                    date: 'long',
                     time: 'short',
                   },
                   'id',
@@ -214,7 +252,12 @@ const closeTagDetail = () => {
             <UButton @click="closeTagDetail" variant="outline" color="gray"
               >Batal</UButton
             >
-            <UButton type="submit">Update</UButton>
+            <UButton
+              :loading="isLoading || tagDetailStatus === 'pending'"
+              type="submit"
+            >
+              {{ Boolean(selectedTagId) ? 'Update' : 'Tambah' }}
+            </UButton>
           </div>
         </UForm>
       </UCard>
