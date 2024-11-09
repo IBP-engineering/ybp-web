aaad399ee0700d7a1f8e5d49cdb73c41f1e41d74
Kalwabed Rizki <49640654+kalwabed@users.noreply.github.com>
2024-11-09 18:45:18 +0700

    feat: initial share story modal

---
+++ app/components/shared/share-story-modal.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const isOpen = defineModel<boolean>('open')
+</script>
+
+<template>
+  <UModal v-model="isOpen">
+    <div class="p-4">
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem velit
+        debitis ratione iste modi cupiditate saepe praesentium, inventore
+        explicabo fugiat reiciendis placeat quod. Maxime, quas quod asperiores
+        eos iure corrupti?
+      </p>
+    </div>
+  </UModal>
+</template>
