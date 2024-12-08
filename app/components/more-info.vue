355fc81ff060724e2b8c0569caa00d11a72c2020
Kalwabed Rizki <49640654+kalwabed@users.noreply.github.com>
2024-12-08 17:31:47 +0700

    Section more info (#39)
    * feat: more info card
    
    * refactor: remove activities
    
    * feat: initial socials sections
    
    * refactor: splitting agenda component
    
    * feat: adding discord
    
    * refactor: set socials to app config
    
    * refactor: using socials in footer

---
+++ app/components/more-info.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const { socials } = useAppConfig()
+</script>
+
+<template>
+  <div class="flex w-full flex-col items-center justify-center">
+    <h3 class="text-xl leading-relaxed">Mari bergabung dan ikuti kami</h3>
+
+    <div class="mt-4 flex w-full flex-wrap justify-center gap-4">
+      <NuxtLink
+        v-for="social in socials"
+        :key="social.link"
+        :to="social.link"
+        class="flex w-fit items-center gap-2 rounded-lg border p-2 font-medium transition"
+        :class="social.theme"
+        target="_blank"
+        :title="social.title"
+        external
+        ><UIcon :name="social.logoFill" class="h-8 w-8" />
+        <span>{{ social.label }}</span></NuxtLink
+      >
+    </div>
+  </div>
+</template>
