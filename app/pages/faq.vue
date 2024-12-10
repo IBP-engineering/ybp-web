65e128f1c5323a879abf1e96e2f806da60fdcc90
Kalwabed Rizki <49640654+kalwabed@users.noreply.github.com>
2024-12-10 20:26:01 +0700

    fix: faq container

--- app/pages/faq.vue
+++ app/pages/faq.vue
@@ -7,7 +7,7 @@ useSeoMeta({
 </script>
 
 <template>
-  <div class="mx-auto w-full max-w-7xl px-4 md:px-0">
+  <div class="container mx-auto px-4 md:px-0">
     <section class="mb-8">
       <h1 class="text-4xl font-bold !leading-relaxed md:text-5xl">FAQ</h1>
       <p class="text-gray-600">
