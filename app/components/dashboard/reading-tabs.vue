9836ab5f74f350463f55864ff973624a88385e8c
Kalwabed Rizki <49640654+kalwabed@users.noreply.github.com>
2025-01-13 21:23:32 +0700

    feat: initial mini staticti

--- app/components/dashboard/reading-tabs.vue
+++ app/components/dashboard/reading-tabs.vue
@@ -20,6 +20,7 @@ const genres = [
 ]
 
 const openConfirmation = ref(false)
+const openNewReadingRecord = ref(false)
 const state = reactive<Schema>({
   title: '',
   genre: 0,
@@ -39,9 +40,54 @@ async function sendNewHabit() {
 
 <template>
   <div>
-    <h2 class="font-medium">Personal Reading Habits</h2>
+    <section class="flex items-center justify-between">
+      <h2 class="font-medium">Personal Reading Habits</h2>
+      <UButton>Tambah baru</UButton>
+    </section>
 
-    <div class="mt-4">
+    <div class="my-4 grid grid-cols-2 gap-4 md:grid-cols-4">
+      <div
+        class="flex flex-col rounded border border-fuchsia-300 bg-fuchsia-50 px-4 py-2 text-fuchsia-900"
+      >
+        <div class="mb-2 flex items-center gap-1">
+          <UIcon name="ph:bowl-steam-duotone" class="h-5 w-5" />
+          <small>GENRE TERBANYAK</small>
+        </div>
+        <p class="text-lg font-semibold">Fiksi</p>
+      </div>
+      <div
+        class="flex flex-col rounded border border-teal-300 bg-teal-50 px-4 py-2 text-teal-900"
+      >
+        <div class="mb-2 flex items-center gap-1">
+          <UIcon name="ph:lamp-pendant-duotone" class="h-5 w-5" />
+          <small>TOTAL RECORD</small>
+        </div>
+        <p class="text-lg font-semibold">12</p>
+      </div>
+      <div
+        class="flex flex-col rounded border border-orange-300 bg-orange-50 px-4 py-2 text-orange-900"
+      >
+        <div class="mb-2 flex items-center gap-1">
+          <UIcon name="ph:clover-duotone" class="h-5 w-5" />
+          <small>TOTAL POIN</small>
+        </div>
+        <p class="text-lg font-semibold">1800</p>
+      </div>
+      <div
+        class="flex flex-col rounded border border-rose-300 bg-rose-50 px-4 py-2 text-rose-900"
+      >
+        <div class="mb-2 flex items-center gap-1">
+          <UIcon name="ph:paragraph-duotone" class="h-5 w-5" />
+          <small>TOTAL HALAMAN</small>
+        </div>
+        <p class="text-lg font-semibold">82</p>
+      </div>
+    </div>
+
+    <UModal v-model="openNewReadingRecord">
+      <template #header>
+        <b>Record Baru</b>
+      </template>
       <UForm
         :schema="v.safeParser(schema)"
         :state="state"
@@ -62,44 +108,48 @@ async function sendNewHabit() {
         <UFormGroup required label="Kesimpulan" name="summary">
           <UTextarea v-model="state.summary" autoresize />
         </UFormGroup>
-
-        <div class="flex justify-end">
-          <UButton type="submit" trailing-icon="ph:floppy-disk-fill"
-            >Simpan</UButton
-          >
-        </div>
       </UForm>
-    </div>
+      <template #footer>
+        <div class="flex justify-end gap-4">
+          <UButton color="gray" variant="soft" @click="openConfirmation = false"
+            >Tutup</UButton
+          >
+          <UButton trailing-icon="ph:floppy-disk-fill">Simpan</UButton>
+        </div>
+      </template>
 
-    <UModal v-model="openConfirmation">
-      <UCard
-        :ui="{
-          ring: '',
-          divide: 'divide-y divide-gray-100 dark:divide-gray-800',
-        }"
-      >
-        <template #header>
-          <b>Tambah baru</b>
-        </template>
+      <UModal v-model="openConfirmation">
+        <UCard
+          :ui="{
+            ring: '',
+            divide: 'divide-y divide-gray-100 dark:divide-gray-800',
+          }"
+        >
+          <template #header>
+            <b>Tambah baru</b>
+          </template>
 
-        <p>
-          Masukan Habit yang sudah ditambahkan hanya bisa diubah maksimal 1
-          kali, dan riwayat yang sudah ditambahkan tidak dapat dihapus.
-        </p>
-        <p>Apakah Anda sudah yakin ingin menambahkannya?</p>
+          <p>
+            Masukan Habit yang sudah ditambahkan hanya bisa diubah maksimal 1
+            kali, dan riwayat yang sudah ditambahkan tidak dapat dihapus.
+          </p>
+          <p>Apakah Anda sudah yakin ingin menambahkannya?</p>
 
-        <template #footer>
-          <div class="flex justify-end gap-4">
-            <UButton
-              color="gray"
-              variant="soft"
-              @click="openConfirmation = false"
-              >Tutup</UButton
-            >
-            <UButton @click="sendNewHabit">OK tambahkan</UButton>
-          </div>
-        </template>
-      </UCard>
+          <template #footer>
+            <div class="flex justify-end gap-4">
+              <UButton
+                color="gray"
+                variant="soft"
+                @click="openConfirmation = false"
+                >Tutup</UButton
+              >
+              <UButton @click="sendNewHabit">OK tambahkan</UButton>
+            </div>
+          </template>
+        </UCard>
+      </UModal>
     </UModal>
+
+    <ReadingHabitTable />
   </div>
 </template>
