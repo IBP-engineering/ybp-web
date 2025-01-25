21be12c1b6fda4ed9bc03d120e956eaf69e56d39
Kalwabed Rizki <49640654+kalwabed@users.noreply.github.com>
2025-01-25 10:54:12 +0700

    feat: initial winners

--- app/pages/reading-habits/leaderboard.vue
+++ app/pages/reading-habits/leaderboard.vue
@@ -7,6 +7,32 @@ useHead({
 })
 
 const selected = ref({ start: sub(new Date(), { days: 7 }), end: new Date() })
+const winners = [
+  {
+    name: 'John',
+    profile: 'https://api.dicebear.com/9.x/glass/svg?seed=John',
+    point: 12000,
+    streakDay: 6,
+  },
+  {
+    name: 'Hendra',
+    profile: 'https://api.dicebear.com/9.x/glass/svg?seed=Hendra',
+    point: 4000,
+    streakDay: 5,
+  },
+  {
+    name: 'Aisyah',
+    profile: 'https://api.dicebear.com/9.x/glass/svg?seed=Aisyah',
+    point: 2400,
+    streakDay: 6,
+  },
+]
+
+const winnerColors = [
+  'border-gold-300 bg-gold-500',
+  'border-silver-300 bg-silver-500',
+  'border-bronze-300 bg-bronze-500',
+]
 </script>
 
 <template>
@@ -21,6 +47,37 @@ const selected = ref({ start: sub(new Date(), { days: 7 }), end: new Date() })
 
     <div class="mx-auto mt-8 flex w-full justify-center">
       <SharedDateRange v-model:selected="selected" />
+    </div>
+
+    <div class="mt-20 grid grid-cols-2 gap-4">
+      <div
+        v-for="(win, key) in winners"
+        :key="win.name"
+        class="flex flex-col items-center text-center"
+        :class="{ 'col-span-2': key === 0 }"
+      >
+        <img
+          :src="win.profile"
+          alt="profile"
+          width="100"
+          height="100"
+          class="rounded-full border"
+        />
+        <b>{{ win.name }}</b>
+        <span
+          class="inline-flex items-center rounded border px-1 py-0.5 text-gray-50"
+          :class="winnerColors[key]"
+          ><UIcon name="ph:crown-simple-fill"
+        /></span>
+        <p>{{ win.streakDay }} hari</p>
+        <div class="mt-1 flex items-center gap-2">
+          <UIcon name="ph:clover-fill" class="-ml-10 h-8 w-8 text-green-600" />
+          <div>
+            <p class="text-2xl font-medium">{{ win.point }}</p>
+            <p class="text-sm text-gray-600">poin</p>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
