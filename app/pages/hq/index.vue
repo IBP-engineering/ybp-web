fb62ddf7ee7b0d9a358e3ff9bb8cdb0b1261469c
Kalwabed Rizki <49640654+kalwabed@users.noreply.github.com>
2025-04-27 05:36:39 +0700

    Pagination (#94)

--- app/pages/hq/index.vue
+++ app/pages/hq/index.vue
@@ -8,33 +8,66 @@ definePageMeta({
   layout: 'hq',
 })
 
+const ITEMS_PER_PAGE = 5
+const page = ref(1)
+
 const supabase = useSupabaseClient<Database>()
-const { data: stories } = await useAsyncData('hq/stories', async () => {
-  const { data, error } = await supabase
-    .from('stories')
-    .select(
-      `*,
+const { data: stories } = await useAsyncData(
+  'hq/stories',
+  async () => {
+    const startIndex = (page.value - 1) * ITEMS_PER_PAGE
+    const endIndex = startIndex + ITEMS_PER_PAGE - 1
+
+    const { data, count, error } = await supabase
+      .from('stories')
+      .select(
+        `*,
       tags:story_tags!id(tag:tag_id(title)),
       author:users(id, username, display_name)
-      `,
-    )
-    .order('created_at', { ascending: false })
+`,
+        { count: 'exact' },
+      )
+      .order('created_at', { ascending: false })
+      .range(startIndex, endIndex)
 
-  if (error) {
-    console.error(error)
-    return []
-  }
+    if (error) {
+      console.error(error)
+      return { items: [], totalPages: 0, totalCount: 0 }
+    }
 
-  return data
-})
+    const totalCount = count || 0
+    const totalPages = Math.ceil(totalCount / ITEMS_PER_PAGE)
+
+    return { items: data, totalPages, totalCount }
+  },
+  {
+    watch: [page],
+    default: () => ({ items: [], totalPages: 0, totalCount: 0 }),
+  },
+)
 </script>
 
 <template>
   <div>
     <PageHeader title="Stories" />
 
-    <div class="mx-auto mt-8 w-full max-w-screen-xl space-y-4 px-4">
-      <HqStoryCard v-for="story in stories" :key="story.id" :story="story" />
+    <div class="mx-auto mt-4 w-full max-w-screen-xl px-4">
+      <p>Total: {{ stories.totalCount }}</p>
+
+      <div class="mx-auto mt-2 w-full max-w-screen-xl px-4 xl:px-0 space-y-4">
+        <HqStoryCard
+          v-for="story in stories.items"
+          :key="story.id"
+          :story="story"
+        />
+      </div>
+      <div class="flex justify-end mt-8 w-full">
+        <UPagination
+          v-model:page="page"
+          :items-per-page="ITEMS_PER_PAGE"
+          :total="stories.totalCount"
+        />
+      </div>
     </div>
   </div>
 </template>
--- app/pages/stories.vue
+++ app/pages/stories.vue
@@ -7,27 +7,44 @@ useSeoMeta({
 })
 
 const supabase = useSupabaseClient()
+const ITEMS_PER_PAGE = 6
+const page = ref(1)
 
-const { data: stories } = await useAsyncData('stories/all', async () => {
-  const { data, error } = await supabase
-    .from('stories')
-    .select(
-      `*,
+const { data: stories } = await useAsyncData(
+  'stories/all',
+  async () => {
+    const startIndex = (page.value - 1) * ITEMS_PER_PAGE
+    const endIndex = startIndex + ITEMS_PER_PAGE - 1
+
+    const { data, error, count } = await supabase
+      .from('stories')
+      .select(
+        `*,
       tags:story_tags!id(tag:tag_id(slug)),
       author:users(id, username, display_name)
       `,
-    )
-    .eq('status', 'approved')
-    .eq('is_active', true)
-    .order('created_at', { ascending: false })
+        { count: 'exact' },
+      )
+      .eq('status', 'approved')
+      .eq('is_active', true)
+      .order('created_at', { ascending: false })
+      .range(startIndex, endIndex)
 
-  if (error) {
-    console.error(error)
-    return []
-  }
+    if (error) {
+      console.error(error)
+      return { items: [], totalpages: 0, totalCount: 0 }
+    }
 
-  return data
-})
+    const totalCount = count || 0
+    const totalPages = Math.ceil(totalCount / ITEMS_PER_PAGE)
+
+    return { items: data, totalPages, totalCount }
+  },
+  {
+    watch: [page],
+    default: () => ({ items: [], totalpages: 0, totalCount: 0 }),
+  },
+)
 
 const breadcrumbs = [
   {
@@ -42,12 +59,12 @@ const breadcrumbs = [
 
 const storiesFiltered = computed(() => {
   // @ts-expect-error move forward for now
-  return mapStoryTag(stories.value)
+  return mapStoryTag(stories.value.items)
 })
 </script>
 
 <template>
-  <div class="container mx-auto w-full px-3 md:px-0">
+  <div class="container mx-auto w-full px-4 xl:px-0">
     <UBreadcrumb
       divider="/"
       :items="breadcrumbs"
@@ -61,7 +78,7 @@ const storiesFiltered = computed(() => {
     </section>
     <div
       v-if="storiesFiltered?.length > 0"
-      class="my-16 grid grid-cols-1 gap-4 md:grid-cols-2"
+      class="my-8 grid grid-cols-1 gap-4 md:grid-cols-2"
     >
       <StoryCard
         v-for="story in storiesFiltered"
@@ -71,5 +88,13 @@ const storiesFiltered = computed(() => {
       />
     </div>
     <p v-else class="mt-10 text-center">Belum ada cerita yang ditambahkan</p>
+
+    <div class="flex justify-end lg:justify-center w-full">
+      <UPagination
+        v-model:page="page"
+        :items-per-page="ITEMS_PER_PAGE"
+        :total="stories.totalCount"
+      />
+    </div>
   </div>
 </template>
