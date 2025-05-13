481acd0a3b8ac2dbc699b5dcb6137184e2d57c43
Kalwabed Rizki <49640654+kalwabed@users.noreply.github.com>
2025-05-13 20:27:27 +0700

    chore: add activities in footer

--- app/components/layout/footer/links.vue
+++ app/components/layout/footer/links.vue
@@ -2,8 +2,8 @@
   <div
     class="grid grid-cols-2 justify-items-stretch gap-4 md:grid-cols-3 md:justify-items-end"
   >
-    <div class="text-neutral-600 md:col-span-2">
-      <p class="mb-2 font-medium">PRODUCTS</p>
+    <section class="text-neutral-600 md:col-span-2">
+      <b class="mb-2 block font-medium">PRODUCTS</b>
       <ul class="space-y-2">
         <li>
           <NuxtLink to="/stories" class="hover:underline">Stories</NuxtLink>
@@ -14,14 +14,20 @@
           >
         </li>
         <li>
+          <NuxtLink to="/activities" class="hover:underline"
+            >Activities</NuxtLink
+          >
+        </li>
+        <li>
           <NuxtLink to="/goodies" class="hover:underline"
             >Goodies (coming soon)</NuxtLink
           >
         </li>
       </ul>
-    </div>
-    <div class="text-neutral-600">
-      <p class="mb-2 font-medium">ORGANIZATION</p>
+    </section>
+
+    <section class="text-neutral-600">
+      <b class="mb-2 block font-medium">ORGANIZATION</b>
       <ul class="space-y-2">
         <li>
           <NuxtLink to="/#about-us" class="hover:underline">About us</NuxtLink>
@@ -33,6 +39,6 @@
           <a href="/#contacts" class="hover:underline">Collaboration</a>
         </li>
       </ul>
-    </div>
+    </section>
   </div>
 </template>
