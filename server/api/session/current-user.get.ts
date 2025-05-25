ea6c84584dcc969ee757b916dd56d7cd032822e7
Kalwabed Rizki <49640654+kalwabed@users.noreply.github.com>
2025-05-25 12:14:55 +0700

    perf: cached user call db

--- server/api/session/current-user.get.ts
+++ server/api/session/current-user.get.ts
@@ -6,19 +6,13 @@ export default defineEventHandler(
   async (
     event,
   ): Promise<Partial<User> & { roles: { id: number; name: string } }> => {
-    const supabase = await serverSupabaseClient<Database>(event)
     const userSession = await serverSupabaseUser(event)
 
     if (!userSession) {
       return null
     }
 
-    const { error, data } = await supabase
-      .from('users')
-      .select('username, id, display_name, created_at, roles(id, name)')
-      .eq('id', userSession.id)
-      .eq('is_active', true)
-      .single()
+    const { data, error } = await getCurrentUser(userSession.id, event)
 
     if (!data) {
       throw createError({
@@ -39,5 +33,25 @@ export default defineEventHandler(
     }
 
     return data
+  },
+)
+
+const getCurrentUser = defineCachedFunction(
+  async (id: string, event) => {
+    const supabase = await serverSupabaseClient<Database>(event)
+    console.log('log', id)
+
+    const { error, data } = await supabase
+      .from('users')
+      .select('username, id, display_name, created_at, roles(id, name)')
+      .eq('id', id)
+      .eq('is_active', true)
+      .single()
+
+    return { data, error }
+  },
+  {
+    name: 'getCurrentUser',
+    maxAge: 60 * 30, // 30 minutes
   },
 )
